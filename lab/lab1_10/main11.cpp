631b48d683a72a8f4d9670e4459ca67d35c8f86c
IvannZ <zhukov.ivan05@yandex.ru>
2024-10-15 13:23:00 +0700

    make hash

--- lab/lab1_10/main11.cpp
+++ lab/lab1_10/main11.cpp
@@ -2,7 +2,19 @@
 #include <stdio.h>
 #include <string>
 
-using namespace std;
+#define SIZE_BUFF_TO_READ 512;
+
+int hash (std::string data){
+    
+    const size_t len = data.length();
+    int hash = 0;
+
+    for (int i = 0; i < len; i++){
+        hash += data[i] * i;
+    }
+
+    return hash;
+}
 
 void print_from_bin_file(FILE *file_read) {
     unsigned char len_line;
@@ -11,7 +23,7 @@ void print_from_bin_file(FILE *file_read) {
         unsigned char c;
         for (int i = 0; i < (int)len_line; i++) {
             if (fread(&c, sizeof(unsigned char), 1, file_read) == 1) {
-                cout << c;
+                std::cout << c;
             } else {
                 return;
             }
@@ -24,6 +36,13 @@ void convert_from_file_to_bin_file(FILE *finput, FILE *foutput) {
     long ptr_to_start_line = ftell(finput);
     int len_line = 0;
     int c;
+
+    
+
+
+    /*
+    
+    
     while ((c = getc(finput)) != EOF) {
         len_line++;
         
@@ -45,33 +64,36 @@ void convert_from_file_to_bin_file(FILE *finput, FILE *foutput) {
             len_line = 0;
         }
     }
+    
+    
+    */
 }
 
 int main(int argc, char* argv[]) {
 
     if (argc != 2) {
-        cout << "Error no file name in argument\n";
+        std::cout << "Error no file name in argument\n";
         return 42;
     }
 
-    string input_filename = argv[1];
+    std::string input_filename = argv[1];
     FILE *finput = fopen(input_filename.c_str(), "r");
     if (finput == NULL) {
-        cout << "Error can't open input file\n";
+        std::cout << "Error can't open input file\n";
         return 52;
     }
 
     if (input_filename.length() < 4) {
-        cout << "Incorrect file name\n";
+        std::cout << "Incorrect file name\n";
         return 94;
     }
-    string output_filename = argv[1];
+    std::string output_filename = argv[1];
     output_filename.erase(output_filename.length() - 4, 4);
     output_filename += ".bin";
 
     FILE *foutput = fopen(output_filename.c_str(), "wb");
     if (foutput == NULL) {
-        cout << "Error can't open output file\n";
+        std::cout << "Error can't open output file\n";
         fclose(finput);
         return 53;
     }
