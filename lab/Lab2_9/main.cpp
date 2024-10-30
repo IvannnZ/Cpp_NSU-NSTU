df42e1955aab62082bb8619b736db09034a10145
IvannZ <zhukov.ivan05@yandex.ru>
2024-10-30 23:19:01 +0700

    add find largest elem in Lab2_9

--- lab/Lab2_9/main.cpp
+++ lab/Lab2_9/main.cpp
@@ -31,19 +31,54 @@ std::map< char, Strings > Text;
 typedef std::vector<std::string> Strings;
 typedef std::map<char, Strings> Text;
 
-Text read_from_file(const std::string &file_name) {
-  std::ifstream file(file_name);
+Text read_from_file(const std::string &file_name);
+
+Strings findLargestVector(const Text& text) {
+  Text::const_iterator maxElement = text.begin();
+
+  for (Text::const_iterator it = text.begin(); it != text.end(); ++it) {
+    if (it->second.size() > maxElement->second.size()) {
+      maxElement = it;
+    }
+  }
+
+  if (maxElement == text.end()) {
+    throw std::runtime_error("Ошибка: структура данных пуста.");
+  }
+
+  return maxElement->second;
+}
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "incorrect number of args";
+    return 42;
+  }
+  Text text;
+  try {
+    text = read_from_file(argv[1]);
+  } catch (char *error) {
+    std::cout << error;
+    return 52;
+  }
+
+  Strings str = findLargestVector(text);
+
+  return 0;
+}
+
+Text read_from_file(const char *file_name) {
+  std::ifstream file((std::string(file_name)));
 
   if (!file.is_open()) {
-    throw std::runtime_error("can`t open file to read";
+    throw std::runtime_error("can`t open file to read");
   }
   Text text;
   std::string str_read;
   while (std::getline(file, str_read)) {
-    text['a'].push_back(str_read);
+    text[str_read[0]].push_back(str_read);
   }
 
-  for (std::pair<const char, std::vector<std::basic_string<char>>> lines :text) {
+  for (std::pair<const char, Strings> lines : text) {
     std::sort(lines.second.begin(), lines.second.end());
   }
 }
