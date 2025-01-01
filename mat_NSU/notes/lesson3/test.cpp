d2a0fa787e12326aa5e8c5b20d8bb499bc47540d
IvannZ <zhukov.ivan05@yandex.ru>
2025-01-02 01:04:30 +0700

    half lesson3

---
+++ mat_NSU/notes/lesson3/test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void fun()
+{
+    std::cout << "in func\n";
+    int a;
+    std::cout << "a:" << a << std::endl;
+    int b[3] = {1};
+    std::cout << "b[0] b[1] b:" << b[0] << ' ' << b[1] << ' ' << b << std::endl;
+    int* c[3];
+    std::cout << "c[0] c:" << c[0] << ' ' << c << std::endl;
+}
+
+int main()
+{
+    int a;
+    std::cout << "a:" << a << std::endl;
+    int b[3] = {1};
+    std::cout << "b[0] b[1] b:" << b[0] << ' ' << b[1] << ' ' << b << std::endl;
+    int* c[3];
+    std::cout << "c[0] c:" << c[0] << ' ' << c << std::endl;
+    fun();
+
+    a = 42;
+    int* ptr = &a;
+    std::cout << ptr << std::endl;
+    ptr -= 5; //
+    std::cout << a << " " << *ptr << std::endl;
+    return 0;
+}
