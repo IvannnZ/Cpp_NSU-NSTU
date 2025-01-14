fc5be65aa6125edcbc1344bb7879ad2c14fb5c49
IvannZ <zhukov.ivan05@yandex.ru>
2025-01-14 20:00:42 +0700

    lesson3 1:10

--- mat_NSU/notes/lesson3/test.cpp
+++ mat_NSU/notes/lesson3/test.cpp
@@ -11,6 +11,30 @@ void fun()
     std::cout << "c[0] c:" << c[0] << ' ' << c << std::endl;
 }
 
+void const_test(){
+    int a = 5;
+    const int const_a = 2;
+    const int *ptr_const_a = &const_a;
+    const int *ptr_a = &a;
+    int *ptr;
+    //ptr = ptr_const_a;
+
+    //*ptr_a = 1;
+    ptr_a = ptr_const_a;
+    //*ptr_const_a = 5;
+    ptr_const_a = ptr;
+
+    ptr_a = &a;
+    int const* const_ptr_a;
+    const_ptr_a = &a;
+    int * const const_ptr_const_a;
+    const_ptr_const_a = &const_a;
+    //const_ptr_a = 1;
+    const_ptr_a = ptr;
+    //*const_ptr_a = 1;
+    const_ptr_const_a = ptr;
+}
+
 int main()
 {
     int a;
